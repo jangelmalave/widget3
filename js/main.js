82623b55ca483b46ba1d839ae769ab565bbfa1db
jangelmalave <jangelmalave@gmail.com>
2019-02-11 12:34:54 -0500

    Funciones básicas JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,96 @@
-var data = { code: 42, items: [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }] };
+data=[
+	{
+		"Categoría": "Merchandising",
+		"Ahorro": 0.1,
+		"Tiempo": 1
+	},
+	{
+		"Categoría": "Material promocional",
+		"Ahorro": 0.1,
+		"Tiempo": 1
+	},
+	{
+		"Categoría": "Material para punto de venta",
+		"Ahorro": 0.1,
+		"Tiempo": 1
+	},
+	{
+		"Categoría": "BTL / Activaciones",
+		"Ahorro": 0.1,
+		"Tiempo": 2
+	},
+	{
+		"Categoría": "Organización de Eventos",
+		"Ahorro": 0.1,
+		"Tiempo": 3
+	},
+	{
+		"Categoría": "Impresiones",
+		"Ahorro": 0.1,
+		"Tiempo": 0.5
+	},
+	{
+		"Categoría": "Avisos/publicidad exterior",
+		"Ahorro": 0.1,
+		"Tiempo": 1
+	},
+	{
+		"Categoría": "Agencias de publicidad / Digitales",
+		"Ahorro": 0.1,
+		"Tiempo": 2
+	},
+	{
+		"Categoría": "Locaciones / salones de eventos",
+		"Ahorro": 0.05,
+		"Tiempo": 2
+	},
+	{
+		"Categoría": "Catering",
+		"Ahorro": 0.05,
+		"Tiempo": 1.5
+	},
+	{
+		"Categoría": "Transporte",
+		"Ahorro": 0.05,
+		"Tiempo": 1
+	},
+	{
+		"Categoría": "Dotaciones",
+		"Ahorro": 0.05,
+		"Tiempo": 2
+	},
+	{
+		"Categoría": "Papelería/material de oficina",
+		"Ahorro": 0.05,
+		"Tiempo": 0.5
+	},
+	{
+		"Categoría": "Cartones y Empaques",
+		"Ahorro": 0.05,
+		"Tiempo": 1
+	},
+	{
+		"Categoría": "Embalajes",
+		"Ahorro": 0.05,
+		"Tiempo": 1
+	},
+	{
+		"Categoría": "Ferretería",
+		"Ahorro": 0.05,
+		"Tiempo": 1
+	}
+];
 
 
-console.log(data.items[1].name);
+var posicion = 0;
+function muestra( option) {
+    posicion = option;
+}
+// Operaciones matemáticas
+var gastoPromedio = 
 
+
+
+$("#action2").click(function(event){
+    alert(data[0].Categoría)
+  });
